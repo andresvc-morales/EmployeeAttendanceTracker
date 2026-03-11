342bd6d15ac56582201fa2e7f4314429e91511f7
andres_morales <andresvcmorales@gmail.com>
2026-03-11 13:11:39 -0600

    Added composer and corrected index extension

---
+++ resources/config/firebase.php
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA13I09OPMLm-tMH9zMQgUFH2abe_rfLM8",
+  authDomain: "employeeattendancetracke-5c502.firebaseapp.com",
+  projectId: "employeeattendancetracke-5c502",
+  storageBucket: "employeeattendancetracke-5c502.firebasestorage.app",
+  messagingSenderId: "858304795754",
+  appId: "1:858304795754:web:9b90d392050a23c783e613"
+};
